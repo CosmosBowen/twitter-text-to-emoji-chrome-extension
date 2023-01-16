25a3fd83c26cf0d29c91cd0bd2669cbe6d35300b
JoanneLeng <1329758457@qq.com>
2023-01-15 21:37:38 -0800

    detect language, only proceed with "en"

--- scripts/new_content.js
+++ scripts/new_content.js
@@ -8,13 +8,15 @@ const observer = new MutationObserver(() => {
 
         for (let i = 0; i < all_tweet_text.length; i++) {
             let each_tweet_text = all_tweet_text[i];
+            const lang = each_tweet_text.getAttribute("lang");
             const all_content_for_each_tweet = each_tweet_text.querySelectorAll('.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0');
-            if (all_content_for_each_tweet.length > 0) {
+            if (lang == "en" && all_content_for_each_tweet.length > 0) {
                 var text_each_tweet = "";
                 for (let j = 0; j < all_content_for_each_tweet.length; j++) {
                     let each_content_for_each_tweet = all_content_for_each_tweet[j];
                     text_each_tweet += each_content_for_each_tweet.textContent;
                 }
+                console.log("lang: ", lang);
                 console.log("text_each_tweet:", text_each_tweet);
             }
         }
